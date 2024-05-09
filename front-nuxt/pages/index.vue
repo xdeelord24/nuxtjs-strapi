7f9f4cc7cda56229af1a4d2b3c044bbf2953c100
xdeelord24 <jgmolinawork@gmail.com>
2024-05-10 05:51:06 +0800

    added strapi logo on interface

--- front-nuxt/pages/index.vue
+++ front-nuxt/pages/index.vue
@@ -2,6 +2,7 @@
 <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
         <v-card class="logo py-4 d-flex justify-center">
+            <StrapiLogo />
             <NuxtLogo />
             <VuetifyLogo />
         </v-card>
