9b469f0bcd0caff39fd21a58adab36522a6bed0b
xdeelord24 <jgmolinawork@gmail.com>
2024-05-10 05:50:46 +0800

    added strapi logo as static

---
+++ front-nuxt/components/StrapiLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <img
+    class="strapi-logo"
+    alt="Strapi Logo"
+    src="/strapi-icon-logo.png"
+  >
+</template>
+
+<style>
+.strapi-logo {
+  height: 180px;
+  width: 180px;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
