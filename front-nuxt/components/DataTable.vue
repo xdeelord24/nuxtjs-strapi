6f088dfacce6a12e044c90819c0b2f9297b84a4e
xdeelord24 <jgmolinawork@gmail.com>
2024-05-13 13:49:17 +0800

    added data table sample

---
+++ front-nuxt/components/DataTable.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-data-table
+      :headers="headers"
+      :items="timeSeriesData"
+      class="elevation-1"
+    ></v-data-table>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Concentration', value: 'attributes.concentration' },
+        { text: 'Date Collected', value: 'attributes.dateCollected' },
+        { text: 'Created At', value: 'attributes.createdAt' },
+        { text: 'Updated At', value: 'attributes.updatedAt' },
+        { text: 'Published At', value: 'attributes.publishedAt' }
+      ],
+    };
+  },
+  computed: {
+    ...mapState({
+      timeSeriesData: state => state.timeSeries.data.data,
+      timeSeriesMeta: state => state.timeSeries.data.meta
+    })
+  },
+  mounted() {
+    this.$store.dispatch('timeSeries/fetchData');
+  }
+};
+</script>
+
