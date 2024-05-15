81037ebe2701687dd56270c83b3304ac1b97bfda
xdeelord24 <jgmolinawork@gmail.com>
2024-05-15 17:16:05 +0800

    added new files from the meeting

---
+++ front-nuxt/pages/dynamic/_id.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>Item Details</h1>
+    <p>Item ID: {{ itemId }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    async asyncData({
+        params
+    }) {
+        const itemId = params.id;
+        // Fetch data based on itemId if needed
+        return {
+            itemId
+        };
+    }
+}
+</script>
---
+++ front-nuxt/pages/newPage/index.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div>
+    Directory Test Page
+    <v-btn to="./secondPage">Second Page</v-btn>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
